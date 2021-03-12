313248fa7072a4eac27b9dc96eb561650b86a3a3
fanta <fanta@LAPTOP-4J3PBLUF>
2021-03-12 11:03:34 +0100

    Creation classes : Personne, Realisateur, Animateur

---
+++ src/projet_fie2/Personne/Animateur.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet_fie2.Personne;
+
+/**
+ *
+ * @author fanta
+ */
+public class Animateur extends Personne{
+
+    public Animateur(String nom, String prenom) {
+        super(nom, prenom);
+    }
+
+    @Override
+    public String toString() {
+        return "Animateur : ";
+    }
+    
+    
+}
--- src/projet_fie2/Personne/Personne.java
+++ src/projet_fie2/Personne/Personne.java
@@ -10,5 +10,17 @@ package projet_fie2.Personne;
  * @author mattonhugo
  */
 public class Personne {
+    private String nom;
+    private String prenom;
+
+    public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    @Override
+    public String toString() {
+        return  nom + " " + prenom;
+    }
     
 }
