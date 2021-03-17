d632aeda2f347ad031864fb2171926b97747dba7
fanta <fanta@LAPTOP-4J3PBLUF>
2021-03-17 08:42:52 +0100

    Rename Grille --> ProgrammeTele

---
+++ src/ProgrammeTele/ProgrammeTele.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProgrammeTele;
+
+import java.util.ArrayList;
+import projet_fie2.Emission.Emission;
+
+/**
+ *
+ * @author mattonhugo
+ */
+public class ProgrammeTele {
+    
+    ArrayList<Emission> lesEmissions;
+    private int[] table;
+    
+    public ProgrammeTele (){
+        this.table = new int[23];
+        this.lesEmissions = new ArrayList<>();
+    }
+    
+    
+}
+
+
