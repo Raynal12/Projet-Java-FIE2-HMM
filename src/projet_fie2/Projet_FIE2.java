31710241cea81c06d051d8fb1d27485e41a207e6
fanta <fanta@LAPTOP-4J3PBLUF>
2021-03-24 12:01:00 +0100

    ToString fonctionnelles pour les 3 classes mais pas encore épurées

--- src/projet_fie2/Projet_FIE2.java
+++ src/projet_fie2/Projet_FIE2.java
@@ -12,8 +12,10 @@ import ProgrammeTele.ProgrammeTele;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import projet_fie2.Emission.Divertissement;
 import projet_fie2.Emission.Reportage;
 import static projet_fie2.Emission.ThemeReportage.ANIMALIER;
+import projet_fie2.Personne.Animateur;
 import projet_fie2.Personne.Realisateur;
 
 /**
@@ -30,11 +32,13 @@ public class Projet_FIE2 {
         ArrayList<String> array = new ArrayList<>();
       
         //new Livre("Le cid", "corneille", 11, 245
-                //int dureeEmission, String nom, ThemeReportage theme, int heureDebut 
-                //int dureeEmission, String nom, int annee, boolean redifusion ,Realisateur realisateur, int heureDebut
+                //Reportage : int dureeEmission, String nom, ThemeReportage theme, int heureDebut 
+                //Fiction : int dureeEmission, String nom, int annee, boolean redifusion ,Realisateur realisateur, int heureDebut
+                //Divertissement : int dureeEmission, String nom, Animateur animateur, int heureDebut
                 
-        new Reportage(1, "Les Lions", ANIMALIER, 14);
-        new Fiction(2, "Avengers", 1920, false, (new Realisateur("Bernard", "Dupont")), 10);
+        System.out.println(new Reportage(1, "Les Lions", ANIMALIER, 15));
+        System.out.println(new Fiction(2, "Avengers", 1920, true, (new Realisateur("Bernard", "Dupont")), 21));
+        System.out.println(new Divertissement(2, "La Famille Pirate", (new Animateur("Jean", "Dupond")), 19));
     }
     
 }
