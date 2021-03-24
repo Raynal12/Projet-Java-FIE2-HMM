ab1f7337c459dc166f551c31f4119a588e306c5f
fanta <fanta@LAPTOP-4J3PBLUF>
2021-03-24 11:22:43 +0100

    Essais ToString

--- src/projet_fie2/Projet_FIE2.java
+++ src/projet_fie2/Projet_FIE2.java
@@ -5,12 +5,15 @@
  */
 package projet_fie2;
 
+import Exception.DureeException;
 import Exception.HoraireException;
 import projet_fie2.Emission.Fiction;
 import ProgrammeTele.ProgrammeTele;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import projet_fie2.Emission.Reportage;
+import static projet_fie2.Emission.ThemeReportage.ANIMALIER;
 import projet_fie2.Personne.Realisateur;
 
 /**
@@ -22,11 +25,16 @@ public class Projet_FIE2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws HoraireException, DureeException {
        
         ArrayList<String> array = new ArrayList<>();
       
-        
+        //new Livre("Le cid", "corneille", 11, 245
+                //int dureeEmission, String nom, ThemeReportage theme, int heureDebut 
+                //int dureeEmission, String nom, int annee, boolean redifusion ,Realisateur realisateur, int heureDebut
+                
+        new Reportage(1, "Les Lions", ANIMALIER, 14);
+        new Fiction(2, "Avengers", 1920, false, (new Realisateur("Bernard", "Dupont")), 10);
     }
     
 }
